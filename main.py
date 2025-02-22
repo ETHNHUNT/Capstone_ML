c825b6a4bcfb2d20768ba4e24b86c5c51b36aaa0
ETHNHUNT <vipinzz001@gmail.com>
2025-02-21 19:15:56 -0500

    main

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ model = joblib.load("xgb_titanic_model.pkl")
 # Initialize FastAPI app
 app = FastAPI()
 
+# Define request schema
+class Passenger(BaseModel):
+    Pclass: int
+    Sex: str
+    Age: float
+    Fare: float
+    SibSp: int
+    Parch: int
+    Embarked: str
+
+# Preprocess input data
+def preprocess_input(data: Passenger):
+    df = pd.DataFrame([data.model_dump()])
+        
+    # Encode categorical variables
+    df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
+    df["Embarked"] = df["Embarked"].map({"S": 0, "C": 1, "Q": 2})
+    
+    # Ensure correct column order
+    features = ["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "Embarked"]
+    return df[features].values
